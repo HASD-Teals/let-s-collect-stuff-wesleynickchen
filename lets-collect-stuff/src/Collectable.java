01620dfe040fb7039f0d46f8d22242333f8878e7
wesleynickchen <wesley.nickchen@gmail.com>
2022-02-28 08:30:14 -0600

    I

--- lets-collect-stuff/src/Collectable.java
+++ lets-collect-stuff/src/Collectable.java
@@ -1,9 +1,83 @@
 public class Collectable {
     // Add collectable properties here
-
+    private int age;
+    private double value;
+    private String name;
+    private boolean broken;
+    private int largeParts;
+    private String color;
     // Add collectable constructors here
 
-    // Add collectable accessors and mutators here
+    public Collectable(){
+        this.age = 17;
+        this.value = 13.22;
+        this.name = "tablet";
+        this.broken = true;
+        this.largeParts = 3;
+        this.color = "red";
+    }
 
+    public Collectable(int age, double value, String name, boolean broken, int largeParts, String color){
+        this.age = age;
+        this.value = value;
+        this.name = name;
+        this.broken = broken;
+        this.largeParts = largeParts;
+        this.color = color;
+    }
+    // Add collectable accessors and mutators here
+    public int getAge() {
+        return this.age;
+    }
+    public double getValue() {
+        return this.value;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public boolean getBroken() {
+        return this.broken;
+    }
+    public int getParts() {
+        return this.largeParts;
+    }
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setValue(double value) {
+        this.value = value;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setBroken(boolean broken) {
+        this.broken = broken;
+    }
+    public void setParts(int largeParts) {
+        this.largeParts = largeParts;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
     // Add any methods here
+    public void losePart() {
+        largeParts -= 1;
+    }
+
+    public void losePart(int x) {
+        largeParts -= x;
+    }
+
+    
+    public String toString() {
+        return (this.age + " " + this.value + " " + this.name + " " + this.broken + " " + this.largeParts + " " + this.color);
+    }
+
+    public boolean equals(Collectable obj) {
+        return (this.age == obj.age && this.value == value && this.name == obj.name && this.broken == obj.broken && this.largeParts == obj.largeParts && this.color == obj.color);
+    }
 }
--- lets-collect-stuff/src/CollectionClient.java
+++ lets-collect-stuff/src/CollectionClient.java
@@ -1,8 +1,61 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class CollectionClient {
     public static void main(String[] args) throws Exception {
         // Add your collection ArrayList here..
         // e.g. List<Collectable> collection = new ArrayList<Collectable>();
-
+        List<Collectable> collection = createList();
+        collection.add(new Collectable(12, 122, "bad among us tablet", false, 1, "red"));
+        collection.add(new Collectable(21, 200, "Ukraine Phone", true, 2, "ground"));
+        collection.add(new Collectable(30, 0, "mom's phone", false, 1, "divorce Blue"));
         // Add collection methods here to manage and manipulate the collection
+        Collectable collectable = new Collectable();
+        collection.add(collectable);
+        printAll(collection);
+        searchList("mom's phone", collection);
+        System.out.println("parts: " + sumationColect(false, collection));
+        System.out.println("value: " + sumationColect(true, collection));
+        collection.sort();
+    }
+
+    public static List<Collectable> createList() {
+        return new ArrayList<Collectable>();
+    }
+
+    public static void printAll(List<Collectable> O) {
+        for (int i = 0; i < O.size(); i++) {
+            System.out.println(O.get(i).toString());
+        }
+    }
+    public static int searchForName(String Tilie, List<Collectable> Olist) {
+        for (int i = 0; i < Olist.size(); i++) {
+            if (Olist.get(i).getName().equals(Tilie)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void searchList(String Tilie, List<Collectable> Olist) {
+        int p = searchForName(Tilie, Olist);
+        if(p >= 0){
+            System.out.println(Tilie +" element found at: " + p);
+        }else{
+            System.out.println("element not found");
+        }
+    }
+
+    public static float sumationColect(boolean byValue, List<Collectable> Olst) {
+        float j = 0;
+        for (int i = 0; i < Olst.size(); i++) {
+            if (byValue) {
+                j += Olst.get(i).getValue();
+            }else{
+                j += Olst.get(i).getParts();
+            }
+        }
+        return j;
     }
 }
