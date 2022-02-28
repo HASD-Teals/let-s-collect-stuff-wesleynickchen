3980f2bd0dc05b6cf9d5f39bc6d79d8ef05fef8a
wesleynickchen <90632650+wesleynickchen@users.noreply.github.com>
2022-02-28 10:06:28 -0600

    sort

--- lets-collect-stuff/src/CollectionClient.java
+++ lets-collect-stuff/src/CollectionClient.java
@@ -2,12 +2,13 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-
+import java.util.jar.Attributes.Name;
+import java.util.Comparator;
 public class CollectionClient {
     public static void main(String[] args) throws Exception {
         // Add your collection ArrayList here..
         // e.g. List<Collectable> collection = new ArrayList<Collectable>();
-        List<Collectable> collection = createList();
+        ArrayList<Collectable> collection = createList();
         collection.add(new Collectable(12, 122, "bad among us tablet", false, 1, "red"));
         collection.add(new Collectable(21, 200, "Ukraine Phone", true, 2, "ground"));
         collection.add(new Collectable(30, 0, "mom's phone", false, 1, "divorce Blue"));
@@ -18,19 +19,20 @@ public class CollectionClient {
         searchList("mom's phone", collection);
         System.out.println("parts: " + sumationColect(false, collection));
         System.out.println("value: " + sumationColect(true, collection));
-        collection.sort();
+        Collections.sort(collection, new SortByVal());
+        System.out.println(collection);
     }
 
-    public static List<Collectable> createList() {
+    public static ArrayList<Collectable> createList() {
         return new ArrayList<Collectable>();
     }
 
-    public static void printAll(List<Collectable> O) {
+    public static void printAll(ArrayList<Collectable> O) {
         for (int i = 0; i < O.size(); i++) {
             System.out.println(O.get(i).toString());
         }
     }
-    public static int searchForName(String Tilie, List<Collectable> Olist) {
+    public static int searchForName(String Tilie, ArrayList<Collectable> Olist) {
         for (int i = 0; i < Olist.size(); i++) {
             if (Olist.get(i).getName().equals(Tilie)) {
                 return i;
@@ -38,7 +40,7 @@ public class CollectionClient {
         }
         return -1;
     }
-    public static void searchList(String Tilie, List<Collectable> Olist) {
+    public static void searchList(String Tilie, ArrayList<Collectable> Olist) {
         int p = searchForName(Tilie, Olist);
         if(p >= 0){
             System.out.println(Tilie +" element found at: " + p);
@@ -47,7 +49,7 @@ public class CollectionClient {
         }
     }
 
-    public static float sumationColect(boolean byValue, List<Collectable> Olst) {
+    public static float sumationColect(boolean byValue, ArrayList<Collectable> Olst) {
         float j = 0;
         for (int i = 0; i < Olst.size(); i++) {
             if (byValue) {
@@ -57,5 +59,13 @@ public class CollectionClient {
             }
         }
         return j;
+    }
+}
+
+class SortByVal implements Comparator<Collectable>
+{
+    public int compare(Collectable a, Collectable b)
+    {
+        return (int)(a.getValue() - b.getValue());
     }
 }
